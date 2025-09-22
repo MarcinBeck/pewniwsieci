581c1fe378b77a70eb37692f608a51db870ab4d3
MarcinBeck <marcin.beck@gmail.com>
2025-09-22 10:36:11 +0200

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,32 @@
+<header id="header">
+  <div id="header-inner">
+    <a href="/#start"><svg id="header-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 840 1000">
+      <path class="cls-1" d="M200,660H0v-200H200v200ZM0,0V340H60v-10c0-160,90-260,260-260h10V790c0,100-40,140-140,140h-30v70H680v-70h-30c-100,0-140-40-140-140V70h10c170,0,260,100,260,260v10h60V0H0ZM640,660h200v-200h-200v200Z"/></svg></a>
+  </div>
+</header>
+<div id="menu-toggle-wrapper" class="menu-toggle-wrapper">
+  <a id="menu-toggle" title="Menu" class="menu-toggle">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+  </a>
+</div>
+<div id="menu-layer">
+  <div id="menu-pink">
+  </div>
+  <div id="main-menu-links">
+      <nav id="main-menu">
+          <ul>
+            <li><a href="/#czy-wiesz-ze" id="czy-wiesz-ze-menu-link">Czy wiesz, że... ?</a></li>
+            <li><a href="/#miniserial" id="cyberkurs-menu-link">Miniserial edukacyjny</a></li>
+            <li><a href="/#nasz-cel" id="nasz-cel-menu-link">Nasz cel</a></li>
+            <!-- <li><a href="/#alerty" id="alerty-menu-link">Alerty</a></li> -->
+            <li><a href="/#baza-wiedzy" id="baza-wiedzy-menu-link">Baza wiedzy</a></li>
+            <li><a href="/#cyberkurs" id="cyberkurs-menu-link">Cyberkurs</a></li>
+            <li><a href="/#nasi-eksperci" id="nasi-eksperci-menu-link">Nasi eksperci</a></li>
+            <li><a href="/#bezpieczenstwo" id="bezpieczenstwo-menu-link">Bezpieczeństwo</a></li>
+            <li><a href="/#projekty-spoleczne" id="projekty-spoleczne-menu-link">Projekty społeczne</a></li>
+          </ul>
+      </nav>
+  </div>
+</div>
